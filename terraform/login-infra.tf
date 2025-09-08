e3ab34cad01761071b20b61aca5fba19892f1965
Tanuja <tatatanuja77@gmail.com>
2025-09-08 15:28:07 +0200

    added network security group name

--- terraform/login-infra.tf
+++ terraform/login-infra.tf
@@ -82,6 +82,7 @@ resource "azurerm_network_security_rule" "login-web-nsg-ssh" {
     destination_address_prefix = "*"
   location            = azurerm_resource_group.login-rg.location
   resource_group_name = azurerm_resource_group.login-rg.name
+  etwork_security_group_name = azurerm_network_security_group.login-rg.name
   }
 
   
