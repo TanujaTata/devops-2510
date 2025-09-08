cdae442f9a10bace7f5ff0ae6d02b52916b309d7
Tanuja <tatatanuja77@gmail.com>
2025-09-08 14:18:30 +0200

    modified

--- terraform/login-infra.tf
+++ terraform/login-infra.tf
@@ -5,9 +5,9 @@ resource "azurerm_resource_group" "login-rg" {
 }
 
 # VNET
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_virtual_network" "login-vnet" {
+  name                = "login-vnet"
+  location            = azurerm_resource_group.login-rg.location
+  resource_group_name = azurerm_resource_group.login-rg.name
   address_space       = ["10.0.0.0/16"]
 }
