956e8b0270066bb8c4a3a75d2df0a7a9d6327b69
Tanuja <tatatanuja77@gmail.com>
2025-09-08 15:40:07 +0200

    http rule added

--- terraform/login-infra.tf
+++ terraform/login-infra.tf
@@ -84,4 +84,18 @@ resource "azurerm_network_security_rule" "login-web-nsg-ssh" {
    network_security_group_name = azurerm_network_security_group.login-web-nsg.name
   }
 
+# web-nsg-http
+resource "azurerm_network_security_rule" "login-web-nsg-http" {
+    name                       = "login-web-nsg-http"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+   resource_group_name = azurerm_resource_group.login-rg.name
+   network_security_group_name = azurerm_network_security_group.login-web-nsg.name
+  }
   
