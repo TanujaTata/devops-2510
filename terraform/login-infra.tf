197f4c3d4e4d0da5a102d978ef28c356d361bd2a
Tanuja <tatatanuja77@gmail.com>
2025-09-08 15:47:35 +0200

    modified

--- terraform/login-infra.tf
+++ terraform/login-infra.tf
@@ -129,7 +129,7 @@ resource "azurerm_network_security_rule" "login-API-nsg-ssh" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "22"
+    destination_port_range     = "80"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
    resource_group_name = azurerm_resource_group.login-rg.name
