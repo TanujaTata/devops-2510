475a7c79523f13de6ec6bc7a0a021c10fb32b211
Tanuja <tatatanuja77@gmail.com>
2025-09-08 14:29:01 +0200

    web-subnet

--- terraform/login-infra.tf
+++ terraform/login-infra.tf
@@ -11,3 +11,12 @@ resource "azurerm_virtual_network" "login-vnet" {
   resource_group_name = azurerm_resource_group.login-rg.name
   address_space       = ["10.0.0.0/16"]
 }
+
+# Web Subnet
+
+resource "azurerm_subnet" "login-web-subnet" {
+  name                 = "login-web-subnet"
+  resource_group_name  = azurerm_resource_group.login-rg.name
+  virtual_network_name = azurerm_virtual_network.login-vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
