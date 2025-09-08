d110c9f18cc6b6455d78843b1716ad781c3c9a67
Tanuja <tatatanuja77@gmail.com>
2025-09-08 16:02:59 +0200

    web nic

--- terraform/login-infra.tf
+++ terraform/login-infra.tf
@@ -173,3 +173,16 @@ resource "azurerm_network_security_rule" "login-db-nsg-http" {
    network_security_group_name = azurerm_network_security_group.login-db-nsg.name
   }
 
+# Web NIC
+resource "azurerm_network_interface" "login-web-nic" {
+  name                = "login-web-nic"
+  location            = azurerm_resource_group.login-rg.location
+  resource_group_name = azurerm_resource_group.login-rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.login-web-subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.login-web-pip.id
+  }
+}
