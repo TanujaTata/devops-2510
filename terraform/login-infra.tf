52f7e45ec1d16a3bdc71b0c73f9f4e91658c6fe2
Tanuja <tatatanuja77@gmail.com>
2025-09-08 15:18:52 +0200

    added ssh rule for web nsg

--- terraform/login-infra.tf
+++ terraform/login-infra.tf
@@ -68,3 +68,20 @@ resource "azurerm_network_security_group" "login-web-nsg" {
   location            = azurerm_resource_group.login-rg.location
   resource_group_name = azurerm_resource_group.login-rg.name
 }
+
+# web-nsg-ssh
+security_rule {
+    name                       = "login-web-nsg-ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
